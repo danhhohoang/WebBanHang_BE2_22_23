eb49089519a5a39102857bb4025537113b1bfcb0
vanbinh2000 <ucvanbinh.tdc2020@gmail.com>
2023-05-14 13:19:04 +0700

    hien thi san pham theo loai tren home cua nguoi dung

--- app/Models/Protype.php
+++ app/Models/Protype.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Protype extends Model
 {
     use HasFactory;
-    protected $table = 'protype';
+    protected $table = 'protypes';
     protected $fillable = [
         'name'
     ];
