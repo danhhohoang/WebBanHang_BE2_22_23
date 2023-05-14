752915c8d50fe9aa734bbaad1bac391ea95cc755
danhhohoang <86829567+danhhohoang@users.noreply.github.com>
2023-05-14 13:05:31 +0700

    ContacUser

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,5 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+//About us
+Route::get('/about-us', [PageControlleroller::class, 'about_us']);
 require __DIR__.'/auth.php';
+//Contact
+Route::get('/contact', [PageController::class, 'contact']);
