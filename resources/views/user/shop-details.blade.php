83cd173bb5ca80eca610ce82731b48e08b50e9a0
baonguyen212002 <baonguyen212002@gmail.com>
2023-05-21 10:02:06 +0700

    Social Media Share Product and Chat Messenger

--- resources/views/user/shop-details.blade.php
+++ resources/views/user/shop-details.blade.php
@@ -48,7 +48,7 @@
                 </div>
                 <div class="col-lg-6 col-md-6">
                     <div class="product__details__text">
-                        <h3>{{ substr($productDetail->name, 0, 20) }}...</h3>
+                        <h3>{{ $productDetail->name}}</h3>
                         <div class="product__details__rating">
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
@@ -58,7 +58,7 @@
                             <span>(18 reviews)</span>
                         </div>
                         <div class="product__details__price">${{ $productDetail->price }}</div>
-                        <p>{{ substr($productDetail->description, 0, 300) }}...</p>
+                        <p>{{ substr($productDetail->description, 0, 300) }}</p>
                         <div class="product__details__quantity">
                             <div class="quantity">
                                 <div class="pro-qty">
@@ -83,10 +83,14 @@
                             <li><b>Weight</b> <span>0.5 kg</span></li>
                             <li><b>Share on</b>
                                 <div class="share">
-                                    <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>
-                                    <a href="#"><i class="fa fa-instagram"></i></a>
-                                    <a href="#"><i class="fa fa-pinterest"></i></a>
+                                    <a href="{{ $shareSocial['facebook'] }}" target="_blank"><i
+                                            class="fa fa-facebook"></i></a>
+                                    <a href="{{ $shareSocial['twitter'] }}" target="_blank"><i
+                                            class="fa fa-twitter"></i></a>
+                                    <a href="{{ $shareSocial['telegram'] }}" target="_blank"><i
+                                            class="fa fa-telegram"></i></a>
+                                    <a href="{{ $shareSocial['pinterest'] }}" target="_blank"><i
+                                            class="fa fa-pinterest"></i></a>
                                 </div>
                             </li>
                         </ul>
